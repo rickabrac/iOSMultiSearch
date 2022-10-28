70f27cb975722702d2947e7b452caa7e58f615f4
Rick Tyler <rick.tyler@gmail.com>
2022-10-28 13:04:32 -0700

    commit

--- MultiSearch/BingResult.swift
+++ MultiSearch/BingResult.swift
@@ -10,6 +10,8 @@ struct BingResult: SearchResult, Hashable {
 	var url: String?
 	var snippet: String?
 
+	// MARK: SearchResult conformance
+
 	var _text: String { guard let _text = snippet else { return "" }; return _text }
 	var _url: String { guard let _url = url else { return "" }; return _url }
 }
