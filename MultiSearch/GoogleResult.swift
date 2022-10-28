5ecd5371d9bef937f7b1e8d7223e47336f3c0e1b
Rick Tyler <rick.tyler@gmail.com>
2022-10-28 15:33:56 -0700

    commit

--- MultiSearch/GoogleResult.swift
+++ MultiSearch/GoogleResult.swift
@@ -7,11 +7,11 @@
 import Foundation
 
 class GoogleResult: SearchResult {
-	var _text: String { guard let text = snippet else { return "" }; return text }
-	var _url: String { guard let url = link else { return "" }; return url }
-
-	// MARK: SearchResult conformance
-	
 	var link: String?
 	var snippet: String?
+	
+	// MARK: SearchResult conformance
+	
+	var _text: String { guard let text = snippet else { return "" }; return text }
+	var _url: String { guard let url = link else { return "" }; return url }
 }
