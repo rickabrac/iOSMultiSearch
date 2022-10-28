1f676f6d574760dcb6f7d08050e37e4e97474233
Rick Tyler <rick.tyler@gmail.com>
2022-10-28 13:14:20 -0700

    commit

--- MultiSearch/GoogleResult.swift
+++ MultiSearch/GoogleResult.swift
@@ -10,7 +10,7 @@ class GoogleResult: SearchResult {
 	var _text: String { guard let text = snippet else { return "" }; return text }
 	var _url: String { guard let url = link else { return "" }; return url }
 
-	// MARK: Codable backend response properties
+	// MARK: SearchResult conformance
 	
 	var link: String?
 	var snippet: String?
